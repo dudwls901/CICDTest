f2f18f3bb442c2416e75da730cc53adea40e11d4
yeongjin <dudwls901@naver.com>
2022-07-18 12:32:14 +0900

    errortest

--- app/src/main/java/com/example/cicdtest/MainActivity.kt
+++ app/src/main/java/com/example/cicdtest/MainActivity.kt
@@ -2,11 +2,13 @@ package com.example.cicdtest
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        errorTest
+        val a = 5
+        Log.d("test", "onCreate: ${a/0}")
     }
 }
